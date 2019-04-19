145303a0b0a81a7db9ae64e2af4881aef9f6931d
kumar bhaskar <bhaskar646@gmail.com>
2019-04-19 14:30:32 +0530

    added function to multiple in log

---
+++ logutils/src/main/java/com/bhaskar/logutils/CustomClass.java
@@ -0,0 +1,11 @@
+package com.bhaskar.logutils;
+
+import android.util.Log;
+
+public class CustomClass {
+
+    private void multi(int a,int b,String TAG)
+    {
+        Log.d(TAG,"mullti of a and b ="+a*b);
+    }
+}
