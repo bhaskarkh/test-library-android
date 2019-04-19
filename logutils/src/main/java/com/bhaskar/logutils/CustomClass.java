ed4b4db4c7f00bd561ae8e71d83f09b7e8645cd6
kumar bhaskar <bhaskar646@gmail.com>
2019-04-19 14:41:18 +0530

    public

--- logutils/src/main/java/com/bhaskar/logutils/CustomClass.java
+++ logutils/src/main/java/com/bhaskar/logutils/CustomClass.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class CustomClass {
 
-    private void multi(int a,int b,String TAG)
+    public void multi(int a,int b,String TAG)
     {
         Log.d(TAG,"mullti of a and b ="+a*b);
     }
