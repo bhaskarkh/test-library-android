0592d58a829ae21b70428066b17b3fe2f26215f9
kumar bhaskar <bhaskar646@gmail.com>
2019-04-19 18:00:52 +0530

    static in mehtod added

--- logutils/src/main/java/com/bhaskar/logutils/CustomClass.java
+++ logutils/src/main/java/com/bhaskar/logutils/CustomClass.java
@@ -4,8 +4,11 @@ import android.util.Log;
 
 public class CustomClass {
 
-    public void multi(int a,int b,String TAG)
+    public static int multi(int a,int b,String TAG)
     {
         Log.d(TAG,"mullti of a and b ="+a*b);
+
+        return a*b;
+
     }
 }
